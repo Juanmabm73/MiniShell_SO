b4b08b37ad6cba48a9c7f882e3ce9a631e48d725
Juanma <juanmabm10@gmail.com>
2024-11-27 19:11:43 +0100

    tokenize, pipes, comienzo decreacion de hijos

--- minishell.c
+++ minishell.c
@@ -1,4 +1,85 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
 
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    int i;
+    int N = 30;                                                             // leer con tokenize
+
+    pid_t pid;
+    int **pids_vector;                                                      // puntero al vector de pids
+    pids_vector = (int *)malloc((N-1) * sizeof(int));                     // reservamos memoria para n-1 pids
+    for (i = 0; i < N; i++){
+        pids_vector[i] = (pid_t *)malloc(sizeof(pid_t));
+    }
+    
+
+    int **pipes_vector;
+    pipes_vector = (int **)malloc((N-1)* sizeof(int));                        // reservamos memoria para N-1 mandatos
+
+
+
+    // dentro de cada hueco para la pipe reservamos memoria para sus dos descriptores
+    for (i = 0; i < N; i++) {
+        pipes_vector[i] = (int *)malloc(2*sizeof(int));
+    }
+
+    // creamos las pipes 
+    for ( i = 0; i < N; i++){
+        pipe(pipes_vector[i]);
+    }
+
+    // ------------------------------------------------------------------------------
+
+    // CREACION DE PROCESOS HIJOS Y LA DE DIOS
+    for (i = 0; i < N; i++){
+        pid = fork();
+
+        if (pid < 0){
+            fprintf(stderr, "Error al crear proceso hijo");
+        } else if (pid == 0) {
+            // implementar logica
+
+        } else {
+
+            // cerramos todos los descriptores de los pipes ya que el padre no usa ninguno
+            for ( i = 0; i < N; i++){
+                close(pipes_vector[i][0]);
+                close(pipes_vector[i][1]);
+            }
+
+            // como buen padre espera a todos sus hijos
+            for (i = 0; i < N; i++){
+                wait(NULL);
+            }
+            
+        }
+        
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    // pendiente hacer free de las pipes y sus descriptores
+
+    return 0;
+}
 
 
 
