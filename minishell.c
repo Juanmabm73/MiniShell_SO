e364b9b2d6ed8abc27d91791c127919f67029dd8
Juanma <juanmabm10@gmail.com>
2024-11-30 14:06:04 +0100

    cierre de pipes procesos hijos etc

--- minishell.c
+++ minishell.c
@@ -7,11 +7,11 @@
 #define MAX_COMMANDS 20
 
 // ---------------------------------------------------------------------------------DEFINIMOS ESTRUCTURA TLINE
-typedef struct
-{
-    char **tokens; // array de punteros a tokens
-    int ncommands; // numero de comandos en la linea
-} tline;
+// typedef struct
+// {
+//     char **tokens; // array de punteros a tokens
+//     int ncommands; // numero de comandos en la linea
+// } tline;
 
 int child_number; // hacemos como en relevos y la i
 
@@ -33,25 +33,25 @@ void sigtstp_handler(int signal)
 }
 
 // ---------------------------------------------------------------------------------TOKENIZAR ENTRADA
-tline *tokenize_input(const char *input)
-{
-    tline *line = malloc(sizeof(tline));                  // reservamos memoria dinamica
-    line->tokens = malloc(sizeof(char *) * MAX_COMMANDS); // reservamos memoria dinamixa para un array de punteros a cadenas
-    line->ncommands = 0;
-    int N = line->ncommands;
+// tline *tokenize_input(const char *input)
+// {
+//     tline *line = malloc(sizeof(tline));                  // reservamos memoria dinamica
+//     line->tokens = malloc(sizeof(char *) * MAX_COMMANDS); // reservamos memoria dinamixa para un array de punteros a cadenas
+//     line->ncommands = 0;
+//     int N = line->ncommands;
 
-    char *input_copy = strdup(input);      // creamos una copia de la entrada para no modificarla
-    char *token = strtok(input_copy, "|"); // creamos un array de tokens donde cada posicion esta separada por un pipe
+//     char *input_copy = strdup(input);      // creamos una copia de la entrada para no modificarla
+//     char *token = strtok(input_copy, "|"); // creamos un array de tokens donde cada posicion esta separada por un pipe
 
-    while (token != NULL && N < MAX_COMMANDS)
-    {
-        line->tokens[N + 1] = strdup(token); // copia el token actual en el que nos encontramos en el array de tokens de line
-        token = strtok(NULL, "|");           // le pasamos NULL para que siga por la posición en la que iba
-    }
+//     while (token != NULL && N < MAX_COMMANDS)
+//     {
+//         line->tokens[N + 1] = strdup(token); // copia el token actual en el que nos encontramos en el array de tokens de line
+//         token = strtok(NULL, "|");           // le pasamos NULL para que siga por la posición en la que iba
+//     }
 
-    free(input_copy);
-    return line;
-}
+//     free(input_copy);
+//     return line;
+// }
 
 // ---------------------------------------------------------------------------------CREAR ARRAY DE PIDS
 int *create_pids_vector(pid_t pid, int N)
@@ -84,6 +84,8 @@ int *create_pipes_vector(int N)
     return pipes_vector;
 }
 
+int j;
+
 // ---------------------------------------------------------------------------------FUNCIÓN MAIN
 int main(int argc, char const *argv[])
 {
@@ -108,8 +110,9 @@ int main(int argc, char const *argv[])
         input[len - 1] = '\0'; // Elimino el caracter
         len--;
     }
-
-    tline *line = tokenize_input(input);
+    // esto creo q no es necesario lo de la funcion porq lo hace directamente la libreria creo 
+    //tline *line = tokenize_input(input);
+    tline *line = tokenize(input); 
     int N = line->ncommands;
 
     //-----------------------------------------------------------------------------
@@ -121,95 +124,78 @@ int main(int argc, char const *argv[])
     // ------------------------------------------------------------------------------
 
     // CREACION DE PROCESOS HIJOS Y LA DE DIOS
-    int j;
-    for (i = 0; i < N; i++)
-    {
-        child_number = i; // guardamos el id del hijo para tenerlo identificado despues de este for
-        pid = fork();
-        if (pid < 0)
-        {
-            fprintf(stderr, "Error al crear proceso hijo");
-        }
-        else if (pid == 0)
-        {
 
-            if (i == 0)
-            { // primer mandato
-                close(pipes_vector[0][0]);
-                dup2(pipes_vector[0][1], STDOUT_FILENO); // redirigimos su salida al extremo de escritura [1] de la primera pipe
-                for (j = 1; j < N - 1; j++)
-                { // cerramos todos los descriptores de las pipes siguientes a la que usa
+    for (i = 0; i < N; i++){
+        child_number = i;                                   // guardamos el id del hijo para tenerlo identificado despues de este for
+        pid = fork();
+        if (pid < 0){
+            fprintf(stderr, "Error al crear proceso hijo \n");
+        } else if (pid == 0) {
+            
+            if (i == 0){                                    // primer mandato
+                close(pipes_vector[0][0]); 
+                dup2(pipes_vector[0][1], STDOUT_FILENO);    // redirigimos su salida al extremo de escritura [1] de la primera pipe
+                for ( j = 1; j < N-1; j++){                 // cerramos todos los descriptores de las pipes siguientes a la que usa
                     close(pipes_vector[j][0]);
                     close(pipes_vector[j][1]);
                 }
-            }
-            else if (i == N - 1)
-            { // ultimo mandato
-                close(pipes_vector[i][1]);
-                dup2(pipes_vector[i][0], STDIN_FILENO);
-                for (j = i; j > 0; j--)
-                {
-                    close(pipes_vector[j][0]);
-                    close(pipes_vector[j][1]);
+            } else if ( i == N-1){                          // ultimo mandato
+                close(pipes_vector[i-1][1]);
+                dup2(pipes_vector[i-1][0], STDIN_FILENO);
+                for ( j = i; j > 0; j--){
+                    close(pipes_vector[j-2][0]);
+                    close(pipes_vector[j-2][1]);
                 }
-            }
-            else
-            {                                  // mandato intermedio
-                close(pipes_vector[i - 1][1]); // de la pipe anterior cierras el extremo de escritura es decir el 1
-                close(pipes_vector[i][0]);     // de la pipe siguiente cierras el de lectura el 0
-                dup2(pipes_vector[i - 1][0], STDIN_FILENO);
+                
+            } else {                                        // mandato intermedio
+                /* 
+
+            [0]         [1]         [2]         [3]         [4]         [5]         [6]         [7]
+                =======     =======     =======     =======     =======     =======     =======    
+                    0           1           2           3           4           5           6
+
+            */
+                close(pipes_vector[i-1][1]);                // de la pipe anterior cierras el extremo de escritura es decir el 1
+                close(pipes_vector[i][0]);                  // de la pipe siguiente cierras el de lectura el 0
+                dup2(pipes_vector[i-1][0], STDIN_FILENO);
                 dup2(pipes_vector[i][1], STDOUT_FILENO);
 
-                // implementar el for que cierre todo el resto de las pipes PENSAR
+                // cerramos las pipes que no utilizamos hasta el inicio
+                for ( j = i; j >= 0 ; j--){
+                    close(pipes_vector[j-2][1]);            // cerramos las pipes que no usa 
+                    close(pipes_vector[j-2][0]); 
+                }
+
+                // cerramos las pipes que no utilizamos hasta el final
+                for (j = i; i < N; j++){
+                    close(pipes_vector[j+2][1]);
+                    close(pipes_vector[j+2][0]);
+                }
             }
-        }
-        else
-        {
+            printf("Todo cerrado y redireccionado con exito vamos con el exec de: %s \n", line->commands[i].filename);
+            execvp(line->commands[i].filename, line->commands[i].argv);
 
-            pids_vector[i] = pid; // nos guardamos el pid del hijo en su posición
+        } else {
+
+             *pids_vector[i] = pid;                  // nos guardamos el pid del hijo en su posición
+            
         }
+        
     }
-
+    
     // cerramos todos los descriptores de los pipes ya que el padre no usa ninguno y si lo cerramos antes los hijos heredan descriptores cerrados cosa que daría fallos
-    for (i = 0; i < N; i++)
-    {
+    for ( i = 0; i < N; i++){
         close(pipes_vector[i][0]);
         close(pipes_vector[i][1]);
     }
 
     // como buen padre espera a todos sus hijos
-    for (i = 0; i < N; i++)
-    {
+    for (i = 0; i < N; i++){
         wait(NULL);
     }
+    
 
     // pendiente hacer free de las pipes y sus descriptores
     return 0;
 }
 
-/*
--  funcion para crear pipes
-    - dependiendo del numero de comandos crea n-1 pipes
-- enlace de pipes y creacion de hijos
-
-    for 0 to len arraymandatos do
-        pid = fork()
-        array pids.push pid
-        if fallo
-        else if == 0 hijo
-            if i == 0 -> mandato inicial
-                cerrar pipe 0 por [0]
-            else if i == len arraymandatos -> final
-                cerrar pipe n-1 por [1] (en caso de que i llegue hasta n-1)
-            else -> intermedios
-                cerrar la pipe de la izquierda por [1] pipe por la que lee
-                cerrar la pope de la derecha por [0] pipe por la que escribe
-        else
-            wait
-
-
-
-
-
-
-*/
